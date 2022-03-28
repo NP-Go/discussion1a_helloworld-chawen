b27f58f6823b8237e725c438526a09bbbfa7f393
Wen Low <chawenmushu@gmail.com>
2022-03-28 12:25:00 +0800

    Added comments and edit hello world changes 28/03

--- Hello.go
+++ Hello.go
@@ -1,12 +1,18 @@
 package main
 
+import "fmt"
+
 func HelloWorld() string {
 	//Insert your code here
 
 	//Do not remove this line here
+	messageHelloWorld := "SHU"
 	return messageHelloWorld
 }
 
 func main() {
 	HelloWorld()
+	// Comment
+	/* Comment 2 */
+	fmt.Println("Hello shu")
 }
